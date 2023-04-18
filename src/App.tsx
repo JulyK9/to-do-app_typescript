618c5c376b09fc452177597659208c332322cb4a
JKKIM <jkesonjk@gmail.com>
2023-04-18 14:16:50 +0900

    todo 추가 로직과 타입 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,21 @@ const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isCompleted: false }]);
+      setTodo('');
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
       <span className="heading">Taskfy</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,13 +3,14 @@ import '../style/styles.css';
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
 // const InputField = ({ todo, setTodo }: {todo: string, setTodo: Function}) => {
 // const InputField = ({ todo, setTodo }: Props) => {
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input
         type="input"
         value={todo}
