0bbfcef83b429d89cfc65a6847ee9721ff8b1507
JKKIM <jkesonjk@gmail.com>
2023-04-18 17:37:05 +0900

    할일 리스트 컴포넌트 로직 작성 및 적용

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,21 @@
+import '../style/styles.css';
+import { Todo } from '../ model';
+import SingleTodo from './SingleTodo';
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
+  return (
+    <div className="todos">
+      {todos.map((todo) => (
+        // <li key={todo.id}>{todo.todo}</li>
+        <SingleTodo todo={todo} todos={todos} setTodos={setTodos} />
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
